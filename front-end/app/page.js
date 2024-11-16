44a32e399eec2c7e24881f383a0b715cbf4e3498
GianfrancoBazzani <bazzanigianfranco@gmail.com>
2024-11-16 16:10:37 +0700

    :shit: chore: rename project LFG! :rocket:

--- front-end/app/page.js
+++ front-end/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="flex h-full w-full bg-gradient-to-r from-purple-500 to-pink-500">
       <main className="flex flex-col w-full gap-8 row-start-2 items-center sm:items-start p-10">
-        <h1 className="text-4xl font-bold text-white">Welcome to Nomadica</h1>
+        <h1 className="text-4xl font-bold text-white">Welcome to MigratoooOooOoOR!</h1>
         <p className="text-xl text-purple-200">
           Migrate your community to Scroll L2 and unlock a universe of
           efficiency.
