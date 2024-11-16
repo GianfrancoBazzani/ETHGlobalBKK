a5c57721887f76874a0a6d3dd4f0145f9c90f536
GianfrancoBazzani <bazzanigianfranco@gmail.com>
2024-11-16 07:20:55 +0700

    Add migration creation pannel

--- front-end/app/migrate/page.js
+++ front-end/app/migrate/page.js
@@ -1,3 +1,136 @@
+"use client";
+
+import { Steps, Step, Button } from "flowbite-react";
+import { Label, TextInput, RangeSlider } from "flowbite-react";
+import { Datepicker } from "flowbite-react";
+
 export default function Migrate() {
-    return <h1>Hello, Next.js!</h1>
-  }
+  return (
+    <div className="flex flex-row">
+      <div className="flex flex-col mt-2 p-4 gap-4 w-1/2">
+        <h2 className="text-2xl font-bold text-purple-300">
+          Migration Creation Menu
+        </h2>
+        {/* To*/}
+        <div className="flex flex-col gap-4">
+          <div>
+            <div className="mb-2 block">
+              <Label
+                className=" text-purple-300 "
+                htmlFor="To"
+                value="L1 ERC20 Token Address"
+              />
+            </div>
+            <TextInput id="To" type="text" sizing="md" />
+          </div>
+        </div>
+
+        {/* Tb*/}
+        <div className="flex flex-col gap-4">
+          <div>
+            <div className="mb-2 block">
+              <Label
+                className=" text-purple-300 "
+                htmlFor="Tb"
+                value="Scroll ERC20 Token Address"
+              />
+            </div>
+            <TextInput id="Tb" type="text" sizing="md" />
+          </div>
+        </div>
+
+        {/* Tr*/}
+        <div className="flex flex-col gap-4">
+          <div>
+            <div className="mb-2 block">
+              <Label
+                className=" text-purple-300 "
+                htmlFor="Tr"
+                value="Scroll ERC20 Rewards Token Address"
+              />
+            </div>
+            <TextInput id="Tr" type="text" sizing="md" />
+          </div>
+        </div>
+
+        {/* Bridge Multiplier*/}
+        <div className=" flex flex-row gap-4 w-full  justify-between pr-4 ">
+          <div className="mb-1 block  ">
+            <Label
+              className=" text-purple-300 "
+              htmlFor="bridge-multiplier"
+              value="Rewards Bridge Multiplier"
+            />
+          </div>
+          <RangeSlider className="" id="bridge-multiplier" /> <div>0.1</div>
+        </div>
+
+        {/* Hold Multiplier*/}
+        <div className=" flex flex-row gap-4 w-full  justify-between pr-4 ">
+          <div className="mb-1 block  ">
+            <Label
+              className=" text-purple-300 "
+              htmlFor="hold-multiplier"
+              value="Rewards Hold Multiplier"
+            />
+          </div>
+          <RangeSlider className="" id="hold-multiplier" /> <div>0.1</div>
+        </div>
+
+        {/* bridge window*/}
+        <div className=" flex flex-row gap-4 w-full  justify-between pr-4 ">
+          <div className="mb-1 block  ">
+            <Label
+              className=" text-purple-300 "
+              htmlFor="bridge-window"
+              value="Bonus Bridge Window"
+            />
+          </div>
+          <div className="flex flex-row gap-4">
+            <Datepicker /> to <Datepicker />
+          </div>
+        </div>
+
+        {/* Hold window*/}
+        <div className=" flex flex-row gap-4 w-full  justify-between pr-4 ">
+          <div className="mb-1 block  ">
+            <Label
+              className=" text-purple-300 "
+              htmlFor="hold-window"
+              value="Hold Window"
+            />
+          </div>
+          <div className="flex flex-row gap-4">
+            <Datepicker /> to <Datepicker />
+          </div>
+        </div>
+        <div className="flex flex-row w-full justify-center">
+          <Button color="purple" className="w-fit ">Create Migration</Button>
+        </div>
+      </div>
+      <div className="flex flex-col mt-2 p-4 gap-4 w-1/2">
+        <h2 className="text-2xl font-bold text-purple-300">
+          Scroll Token Deployer menu
+        </h2>
+        <div className="flex flex-col h-full w-full  justify-center content-center items-center">
+          <Button  color="purple" className="w-fit ">Deploy Token</Button>
+          <div className="w-full">
+            {/* Tb deploy*/}
+            <div className="flex flex-col gap-4">
+              <div>
+                <div className="mb-2 block">
+                  <Label
+                    className=" text-purple-300 "
+                    htmlFor="tb-deploy"
+                    value="Scroll Token Address"
+                  />
+                </div>
+                <TextInput id="Tb-deploy" type="text" sizing="md" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
