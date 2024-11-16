14e627ada447ee3e5f6e6e07b4e26bf6994acec3
Vladimir Kumalagov <vkumalagov@icloud.com>
2024-11-17 05:09:59 +0700

    feat: add user information and validate the completeMigration

--- contracts/src/vlayer/MigratorVerifier.sol
+++ contracts/src/vlayer/MigratorVerifier.sol
@@ -5,30 +5,42 @@ import {Verifier, Proof} from "vlayer-0.1.0/Verifier.sol";
 import {MigratorProver} from "./MigratorProver.sol";
 
 import {IERC20, SafeERC20} from "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
+import {SafeCast} from "openzeppelin-contracts/utils/math/SafeCast.sol";
 import {IMigratorVerifier} from "./interfaces/IMigratorVerifier.sol";
 
 contract MigratorVerifier is Verifier, IMigratorVerifier {
     using SafeERC20 for IERC20;
 
-    uint256 private constant ONE_SCALED = 1e18;
-    uint256 private constant MINIMAL_MIGRATION_DURATION = 1 days;
-    uint256 private constant BASE = 10000;
-
-    address private immutable prover;
-    IERC20 private immutable l2Token;
-    uint256 private immutable baseTimeMultiplier;
-    uint256 private immutable totalMigrationDuration;
-    uint256 private immutable startMigrationTimestamp;
-    uint256 private immutable endMigrationTimestamp;
-
-    BonusRange[] private bonusRanges;
+    struct UserInfo {
+        bool isMigrated;
+        uint248 usedLockedAmount;
+    }
 
     struct BonusRange {
         uint184 rangeStartAmount;
         uint72 multiplier;
     }
 
+    // *** Constants & Immutables *** \\\
+
+    uint256 private constant ONE_SCALED = 1e18;
+    uint256 private constant MINIMAL_MIGRATION_DURATION = 1 days;
+
+    address private immutable prover;
+    address private immutable l1Migrator;
+    IERC20 private immutable l2Token;
+    uint256 private immutable baseTimeMultiplier;
+    uint256 private immutable totalMigrationDuration;
+    uint256 private immutable startMigrationTimestamp;
+    uint256 private immutable endMigrationTimestamp;
+
+    // *** State Variables *** \\\
+    BonusRange[] private bonusRanges;
+    mapping(address user => UserInfo info) private userMigrationInformation;
+
+    // *** State Changing Functions *** \\\
     constructor(
+        address _l1Migrator,
         uint256 _startMigrationTimestamp,
         uint256 _endMigrationTimestamp,
         uint256 _baseTimeMultiplier,
@@ -39,6 +51,7 @@ contract MigratorVerifier is Verifier, IMigratorVerifier {
         uint256 _minimalAverageBalanceForBonusEligibility,
         BonusRange[] memory _bonusRanges
     ) {
+        // *** Validations *** \\\
         require(_baseTimeMultiplier > 0, ZeroBaseMultiplier());
         require(
             _startMigrationTimestamp >= block.timestamp,
@@ -48,12 +61,13 @@ contract MigratorVerifier is Verifier, IMigratorVerifier {
             _endMigrationTimestamp > _startMigrationTimestamp,
             InvalidMigrationEnd(_startMigrationTimestamp, _endMigrationTimestamp)
         );
-
-        uint256 migrationDuration = _endMigrationTimestamp - _startMigrationTimestamp;
-        require(migrationDuration > MINIMAL_MIGRATION_DURATION, MigrationDurationTooSmall());
+        require(_l1Migrator != address(0), L1MigratorMustBeNotZero());
 
         // Sanity check that token exists
         _l2Token.totalSupply();
+
+        uint256 migrationDuration = _endMigrationTimestamp - _startMigrationTimestamp;
+        require(migrationDuration > MINIMAL_MIGRATION_DURATION, MigrationDurationTooSmall());
 
         // If project wants to set the bonus based on average balance to the constant for every user
         // then they will provide array with 1 element where the rangeStartAmount is 0 and the multiplier is some value
@@ -67,6 +81,7 @@ contract MigratorVerifier is Verifier, IMigratorVerifier {
             );
         }
 
+        // *** State changes *** \\\
         // Deploy the MigrationProver first to validate all parameters first
         prover = address(
             new MigratorProver(
@@ -84,6 +99,7 @@ contract MigratorVerifier is Verifier, IMigratorVerifier {
 
         bonusRanges = _bonusRanges;
         l2Token = _l2Token;
+        l1Migrator = _l1Migrator;
     }
 
     function completeMigration(Proof memory, address user, uint256 averageBalance)
@@ -91,32 +107,26 @@ contract MigratorVerifier is Verifier, IMigratorVerifier {
         onlyVerified(prover, MigratorProver.averageBalance.selector)
     {
         require(msg.sender == user, ProofIsNotGeneratedForCaller(user, msg.sender));
+        require(!userMigrationInformation[msg.sender].isMigrated, UserAlreadyMigrated());
         require(endMigrationTimestamp > block.timestamp, BonusMigrationEnded());
 
         uint256 timeBonus = getUserTimeBonus(averageBalance);
         uint256 loyaltyBonus = getUserTimeBonus(averageBalance);
 
         uint256 l1LockedAmount = 0; // Read it from the L1
+
+        // Invariant should never happen
+        assert(l1LockedAmount >= userMigrationInformation[msg.sender].usedLockedAmount);
         uint256 totalAmountToSend = l1LockedAmount + timeBonus + loyaltyBonus;
+
+        userMigrationInformation[msg.sender].isMigrated = true;
+        userMigrationInformation[msg.sender].usedLockedAmount +=
+            SafeCast.toUint248(l1LockedAmount - userMigrationInformation[msg.sender].usedLockedAmount);
+
         l2Token.safeTransfer(msg.sender, totalAmountToSend);
     }
 
-    function getUserTimeBonus(uint256 usersAverageBalance) public view returns (uint256) {
-        return (usersAverageBalance * getTimeMultiplier()) / ONE_SCALED;
-    }
-
-    function getUserLoyaltyBonus(uint256 usersAverageBalance) public view returns (uint256) {
-        return (usersAverageBalance * findMultiplierForValue(usersAverageBalance)) / ONE_SCALED;
-    }
-
-    function getTimeMultiplier() private view returns (uint256) {
-        uint256 remainingMigrationTime = endMigrationTimestamp - block.timestamp;
-
-        uint256 numerator = baseTimeMultiplier * remainingMigrationTime * ONE_SCALED;
-        uint256 fraction = (numerator / totalMigrationDuration) / ONE_SCALED;
-
-        return ONE_SCALED + fraction;
-    }
+    // *** Private View Functions *** \\\
 
     function findMultiplierForValue(uint256 value) private view returns (uint256 multiplier) {
         uint256 arrayLengthCached = bonusRanges.length;
@@ -159,7 +169,30 @@ contract MigratorVerifier is Verifier, IMigratorVerifier {
         revert InvalidState();
     }
 
+    // *** External & Public Getter Functions *** \\\
+
     function getBonusRanges() external view returns (BonusRange[] memory) {
         return bonusRanges;
+    }
+
+    function getUserInformation(address user) external view returns (UserInfo memory) {
+        return userMigrationInformation[user];
+    }
+
+    function getUserTimeBonus(uint256 usersAverageBalance) public view returns (uint256) {
+        return (usersAverageBalance * getTimeMultiplier()) / ONE_SCALED;
+    }
+
+    function getUserLoyaltyBonus(uint256 usersAverageBalance) public view returns (uint256) {
+        return (usersAverageBalance * findMultiplierForValue(usersAverageBalance)) / ONE_SCALED;
+    }
+
+    function getTimeMultiplier() public view returns (uint256) {
+        uint256 remainingMigrationTime = endMigrationTimestamp - block.timestamp;
+
+        uint256 numerator = baseTimeMultiplier * remainingMigrationTime * ONE_SCALED;
+        uint256 fraction = (numerator / totalMigrationDuration) / ONE_SCALED;
+
+        return ONE_SCALED + fraction;
     }
 }
